0a411df2ecd98c52e8bc34c7b0a638d994aa8690
CosLynx AI <drixisgod@gmail.com>
2025-05-27 02:18:49 +0530

    generated file: `src/vite-env.d.ts` [base-template]

---
+++ src/vite-env.d.ts
@@ -0,0 +1,72 @@
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_APP_TITLE: string
+  // more env variables...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
+
+declare module '*.module.css' {
+  const classes: { readonly [key: string]: string }
+  export default classes
+}
+
+declare module '*.module.scss' {
+  const classes: { readonly [key: string]: string }
+  export default classes
+}
+
+declare module '*.module.sass' {
+  const classes: { readonly [key: string]: string }
+  export default classes
+}
+
+declare module '*.module.less' {
+  const classes: { readonly [key: string]: string }
+  export default classes
+}
+
+declare module '*.module.styl' {
+  const classes: { readonly [key: string]: string }
+  export default classes
+}
+
+declare module '*.png' {
+  const value: any;
+  export default value;
+}
+
+declare module '*.jpg' {
+  const value: any;
+  export default value;
+}
+
+declare module '*.jpeg' {
+  const value: any;
+  export default value;
+}
+
+declare module '*.svg' {
+  import React = require('react');
+  export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+  const src: string;
+  export default src;
+}
+
+declare module '*.mp3' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.wav' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.ogg' {
+  const src: string;
+  export default src;
+}
